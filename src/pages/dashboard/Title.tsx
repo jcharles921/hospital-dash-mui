c582ca86e9be4defd38981c0e2b4664cffa13bac
jcharles921 <babouwitonze921@gmail.com>
2024-04-21 23:35:56 +0200

    adding login and logout

--- src/pages/dashboard/Title.tsx
+++ src/pages/dashboard/Title.tsx
@@ -5,16 +5,18 @@ interface TitleProps {
   children?: React.ReactNode;
   variant?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
   component?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+  sx?: React.CSSProperties;
 }
 
 export default function Title(props: TitleProps) {
-  const { component = "h2", variant = "h6" } = props;
+  const { component = "h2", variant = "h6", sx } = props;
 
   return (
     <Typography
       component={component}
       variant={variant}
       color="primary"
+      sx={sx}
       gutterBottom
     >
       {props.children}
